7363d35b2fd00acfcebf51a34fcaad939e5c616d
makiri <kireew.martin@icloud.com>
2020-04-28 16:03:43 +0200

    remove unnecessary files

--- timesheet/dist/timesheet.d.ts
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { TymeEntry } from "./models/TymeEntry";
-interface TimeSheetProps {
-    tymeEntries: TymeEntry[];
-}
-declare const TimeSheet: FC<TimeSheetProps>;
-export default TimeSheet;
-//# sourceMappingURL=timesheet.d.ts.map
